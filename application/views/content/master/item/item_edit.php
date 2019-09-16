74e5db31c3a3407e7ec8f64cc9a0b97d32bc5203
Willy <willy@Willys-MacBook-Pro.local>
2019-09-16 15:00:11 +0700

    coba commit

--- application/views/content/master/item/item_edit.php
+++ application/views/content/master/item/item_edit.php
@@ -1,4 +1,5 @@
 <div class="x_panel">
+    <!-- Coba Commit -->
     <div class="x_title">
         <h2><?= $panel_title ?></h2>
         <ul class="nav navbar-right panel_toolbox">
@@ -22,49 +23,49 @@
 
         <?= form_open_multipart($url_controller . "submit", ' id="form-1" class="form-horizontal form-label-left" ') ?>
         <div class="col-md-6">
-            <?= form_hidden('id_item', $id_item)?>
+            <?= form_hidden('id_item', $id_item) ?>
             <div class="form-group">
                 <label class="col-md-4" for="nama"> <span class="required">*</span>
                     Nama
                 </label>
                 <div class="col-md-8">
-                    <input type="text" id="nama_item" name="nama_item" value="<?=$nama_item?>"  class="form-control col-md-7 col-xs-12">
+                    <input type="text" id="nama_item" name="nama_item" value="<?= $nama_item ?>" class="form-control col-md-7 col-xs-12">
                 </div>
             </div>
-            
+
             <div class="form-group">
                 <label class="col-md-4" for="satuan"> <span class="required">*</span>
                     Satuan
                 </label>
                 <div class="col-md-8">
-                    <?= form_dropdown('satuan', $opt_satuan, $satuan , ' class="form-control" id="satuan" ')?>
+                    <?= form_dropdown('satuan', $opt_satuan, $satuan, ' class="form-control" id="satuan" ') ?>
                 </div>
             </div>
-            
+
             <div class="form-group">
                 <label class="col-md-4" for="satuan"> <span class="required">*</span>
                     Punya Stok
                 </label>
                 <div class="col-md-8">
-                    <?= form_checkbox('is_have_stock', '1', $is_have_stock, '  ')?>
+                    <?= form_checkbox('is_have_stock', '1', $is_have_stock, '  ') ?>
                 </div>
             </div>
 
         </div>
         <div class="col-md-6">
-            
+
         </div>
-        <div class="col-md-12">                
+        <div class="col-md-12">
             <div class="col-md-8">
                 <?= form_submit('save', 'save', ' id="save" class="btn btn-primary" ') ?>
-                <a href="<?=base_url().$url_controller?>" class="btn btn-default">Cancel</a>
+                <a href="<?= base_url() . $url_controller ?>" class="btn btn-default">Cancel</a>
             </div>
         </div>
         <?= form_close() ?>
     </div>
 </div>
 <style>
-    .fixed-alert{
+    .fixed-alert {
         position: fixed;
         z-index: 9999;
         width: 300px;
@@ -72,29 +73,29 @@
         right: 10px;
     }
 </style>
-<div id="alert-error" class="alert alert-danger fixed-alert" style="display: none;" >
+<div id="alert-error" class="alert alert-danger fixed-alert" style="display: none;">
     <strong>Alert !</strong>
     <p id="alert-error-html"></p>
 </div>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         $('#regencies_id').select2({
             placeholder: 'Ketik Nama Kota',
             ajax: {
                 url: '<?= base_url($url_controller . "kota_ajax") ?>',
-                
+
             },
             allowClear: true,
         });
 
-        $('#alert-error').click(function () {
+        $('#alert-error').click(function() {
             $(this).hide();
         });
 
 
     });
-    $('#form-1').submit(function (e) {
+    $('#form-1').submit(function(e) {
         e.preventDefault();
 
         $('#save').prop('disabled', true);
@@ -106,12 +107,12 @@
             contentType: false, // The content type used when sending data to the server.
             cache: false, // To unable request pages to be cached
             processData: false, // To send DOMDocument or non processed data file it is set to false
-            success: function (data)   // A function to be called if request succeeds
+            success: function(data) // A function to be called if request succeeds
             {
                 if (!data.status) {
                     $('#alert-error').show();
                     $('#alert-error-html').html(data.message);
-                    setTimeout(function () {
+                    setTimeout(function() {
                         $('#alert-error').fadeOut('slow');
                     }, 2000);
                 } else {
@@ -120,7 +121,7 @@
                 $('#save').prop('disabled', false);
 
             },
-            error: function (err) {
+            error: function(err) {
                 alert(JSON.stringify(err));
                 $('#save').prop('disabled', false);
             }
