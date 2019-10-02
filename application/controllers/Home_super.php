18d7d573a46af5a080dcd9495dd64dd2bbf518fe
Willy <willy@Willys-MacBook-Pro.local>
2019-10-02 10:30:45 +0700

    test

--- application/controllers/Home_super.php
+++ application/controllers/Home_super.php
@@ -17,7 +17,7 @@ class Home_super extends CI_Controller
         $auth_sa = new Auth_super_user_model();
         $auth_sa->is_logged_in();
     }
-    //test
+    //test hahahaha
 
     public function index()
     {
