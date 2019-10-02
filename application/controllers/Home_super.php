0a71395cc2b3d7d0ff093718dd0ae24f5f38816f
haris <harits.wahyu.hid@gmail.com>
2019-10-02 15:22:19 +0700

    super user redirect ke dashboard

--- application/controllers/Home_super.php
+++ application/controllers/Home_super.php
@@ -2,14 +2,12 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home_super extends CI_Controller
-{
+class Home_super extends CI_Controller {
 
     private $title = "Dashboard";
     private $url_controller = 'Home_super';
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         $this->load->library('session');
 
@@ -17,11 +15,8 @@ class Home_super extends CI_Controller
         $auth_sa = new Auth_super_user_model();
         $auth_sa->is_logged_in();
     }
-    
-    //coba haris branch_h
 
-    public function index()
-    {
+    public function index() {
         $app_model = new Application_model();
         $app = $app_model->get_all_data();
 
@@ -36,6 +31,7 @@ class Home_super extends CI_Controller
         $business_data = $this->db->get('m_business')->result_array();
 
         $data_contents['business_data'] = $business_data;
+        $data_contents['url_controller'] = $this->url_controller;
 
 
         $content = $this->load->view('home_super/dashboard_super', $data_contents, true);
@@ -52,5 +48,39 @@ class Home_super extends CI_Controller
             'js_files' => $js_files
         );
         $this->load->view('template_owner_dashboard', $template);
+    }
+
+    public function tambah_business() {
+        $app_model = new Application_model();
+        $app = $app_model->get_all_data();
+
+        $this->load->helper('haris_helper');
+        $data_contents = array();
+        $data_contents['panel_title'] = $this->title . "";
+        $data_contents['url_controller'] = $this->url_controller;
+        
+
+        $user_super = new Auth_super_user_model();
+        $user_data = $user_super->get_userdata();
+        
+        $content = $this->load->view('home_super/tambah_business', $data_contents, true);
+
+        $css_files = array();
+        $js_files = array();
+
+        $template = array(
+            'url_controller' => $this->url_controller,
+            'content' => $content,
+            'page_title' => $this->title,
+            'content_title' => $this->title,
+            'css_files' => $css_files,
+            'js_files' => $js_files
+        );
+        $this->load->view('template_owner_dashboard', $template);
+    }
+
+    
+    public function submit_add_business(){
+        
     }
 }
